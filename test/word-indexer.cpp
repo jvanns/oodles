dff2602dc05123daa66381e0e43dd61e17cf685d
James Vanns <james.vanns@gmail.com>
2010-03-01 17:29:09 +0000

    test/word-indexer: Allow word-indexer to take multiple file paths on the command-line.
    Use more unit test-like I/O formatting ready for Boost.test (when I get round to that).

--- test/word-indexer.cpp
+++ test/word-indexer.cpp
@@ -18,22 +18,16 @@ using std::string;
 // STL exception
 using std::exception;
 
-int main(int argc, char *argv[]) {
-    if (!argv[1]) {
-        cerr << "Provide the location (file path) to a raw, ASCII text file.\n";
-        return 1;
-    }
-
+static
+bool
+tokenise_words(const string &path)
+{
     try {
         string s;
-        int n = oodles::read_file_data(argv[1], s);
+        int n = oodles::read_file_data(path, s);
 
-        if (n != static_cast<int>(s.size())) {
-            cerr << "No. bytes read into buffer not equal to size of input.\n";
-            return 1;
-        }
-
-        cout << "Read " << n << " bytes from " << argv[1] << ".\n";
+        if (n != static_cast<int>(s.size()))
+            return false;
 
         string word;
         oodles::DistinctItemVector<string> words;
@@ -42,8 +36,7 @@ int main(int argc, char *argv[]) {
         for (n = 0 ; i != j ; ++i) {
             if (isspace(*i)) {
                 if (!word.empty()) {
-                    cout << "Appended '" << word << "' at index "
-                         << words.append_item(word) << endl;
+                    words.append_item(word);
                     word.clear();
                     ++n;
                 }
@@ -51,19 +44,36 @@ int main(int argc, char *argv[]) {
                 word += *i;
         }
 
-        if (n != static_cast<int>(words.sequence_count())) {
-            cerr << "No. items appended not equal to no. of input items.\n";
-            return 1;
-        }
-
-        for (uint32_t p = 0 ; p < words.sequence_count() ; ++p) {
-            words.retrieve_item(p, word);
-            cout << word << endl;
-        }
+        if (n != static_cast<int>(words.sequence_count()))
+            return false;
     } catch (const exception &e) {
         cerr << e.what() << endl;
+        return false;
+    }
+
+    return true;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        cerr << "Provide the location (file path) to a raw, ASCII text file.\n";
         return 1;
     }
 
-    return 0;
+    int rc = 0;
+
+    for (int i = 1 ; i < argc ; ++i) {
+        cout << "DistinctItemVector test#" << i << " (" << argv[i] << ")...";
+
+        if (tokenise_words(argv[i])) {
+            cout << "PASS.";
+        } else {
+            cout << "FAIL.";
+            rc = 1;
+        }
+
+        cout << endl;
+    }
+
+    return rc;
 }
