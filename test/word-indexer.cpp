42ecf91feb8649f48e8ebd06a87e437857940647
James Vanns <james.vanns@gmail.com>
2010-03-01 15:33:24 +0000

    test/word-indexer: Update word-indexer to catch exceptions.

--- test/word-indexer.cpp
+++ test/word-indexer.cpp
@@ -15,52 +15,55 @@ using std::endl;
 // Containers
 using std::string;
 
+// STL exception
+using std::exception;
+
 int main(int argc, char *argv[]) {
     if (!argv[1]) {
         cerr << "Provide the location (file path) to a raw, ASCII text file.\n";
         return 1;
     }
 
-    string s;
-    int n = read_file_data(argv[1], s);
+    try {
+        string s;
+        int n = read_file_data(argv[1], s);
 
-    if (n == -1) {
-        cerr << "Failed to open " << argv[1] << " for reading.\n";
+        if (n != static_cast<int>(s.size())) {
+            cerr << "No. bytes read into buffer not equal to size of input.\n";
+            return 1;
+        }
+
+        cout << "Read " << n << " bytes from " << argv[1] << ".\n";
+
+        string word;
+        oodles::DistinctItemVector<string> words;
+        string::const_iterator i = s.begin(), j = s.end();
+
+        for (n = 0 ; i != j ; ++i) {
+            if (isspace(*i)) {
+                if (!word.empty()) {
+                    cout << "Appended '" << word << "' at index "
+                         << words.append_item(word) << endl;
+                    word.clear();
+                    ++n;
+                }
+            } else
+                word += *i;
+        }
+
+        if (n != static_cast<int>(words.sequence_count())) {
+            cerr << "No. items appended not equal to no. of input items.\n";
+            return 1;
+        }
+
+        for (uint32_t p = 0 ; p < words.sequence_count() ; ++p) {
+            words.retrieve_item(p, word);
+            cout << word << endl;
+        }
+    } catch (const exception &e) {
+        cerr << e.what() << endl;
         return 1;
     }
 
-    if (n != static_cast<int>(s.size())) {
-        cout << "No. bytes read into buffer not equal to size of input.\n";
-        return 1;
-    }
-
-    cout << "Read " << n << " bytes from " << argv[1] << ".\n";
-
-    string word;
-    oodles::DistinctItemVector<string> words;
-    string::const_iterator i = s.begin(), j = s.end();
-
-    for (n = 0 ; i != j ; ++i) {
-        if (isspace(*i)) {
-            if (!word.empty()) {
-                cout << "Appended '" << word << "' at index "
-                     << words.append_item(word) << endl;
-                word.clear();
-                ++n;
-            }
-        } else
-            word += *i;
-    }
-
-    if (n != static_cast<int>(words.sequence_count())) {
-        cerr << "Total no. items appended not equal to no. of input items.\n";
-        return 1;
-    }
-
-    for (uint32_t p = 0 ; p < words.sequence_count() ; ++p) {
-        words.retrieve_item(p, word);
-        cout << word << endl;
-    }
-
-    return 1;
+    return 0;
 }
