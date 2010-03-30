a804f12e96dab8f3c6b1a8f245f7b2ed807addf7
James Vanns <james.vanns@gmail.com>
2010-03-30 19:25:22 +0100

    utility/math: Start math utility functions. Add normalise().
    Returns the scale of the arbitrary value within a specified range.

---
+++ utility/math.hpp
@@ -0,0 +1,18 @@
+#ifndef OODLES_MATH_HPP
+#define OODLES_MATH_HPP
+
+namespace oodles {
+
+template<class T>
+float normalise(const T &n, // Arbitrary value
+                const T &to_min, // Target range
+                const T &to_max,
+                const T &from_min, // Source range
+                const T &from_max);
+
+} // oodles
+
+#include "math.ipp" // Implementation
+
+#endif
+
