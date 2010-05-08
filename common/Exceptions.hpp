ecbc472639290168c34119a3ddd15ba4f014a281
James Vanns <james.vanns@gmail.com>
2010-05-08 10:45:57 +0100

    common/Exceptions: Add new ParserError exception.
    For use with url/Parser and perhaps later, html/Parser.

--- common/Exceptions.hpp
+++ common/Exceptions.hpp
@@ -6,11 +6,21 @@
 
 namespace oodles {
 
+// General oodles exceptions
 struct ReadError : public NonFatalException
 {
     ReadError(const std::string &from, int error, const char *format, ...);
 };
 
+// URL-specific exceptions
+namespace url {
+
+struct ParseError : public NonFatalException
+{
+    ParseError(const std::string &from, int error, const char *format, ...);
+};
+
+} // url
 } // oodles
 
 #endif
