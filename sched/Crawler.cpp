0cb8cb1249875363548ba555ee78b592eb4f3bfd
James Vanns <james.vanns@gmail.com>
2010-05-26 19:18:33 +0100

    sched/Crawler: Be more explicit in RankCrawler.
    Add a few more comments to make it understandable what we
    want achieve.

--- sched/Crawler.cpp
+++ sched/Crawler.cpp
@@ -30,18 +30,28 @@ bool
 RankCrawler::operator() (const Crawler *lhs, const Crawler *rhs) const
 {
     /*
-     * First, be sure to check that the Crawler has a valid Endpoint!
+     * First, be sure to check that the Crawlers have a valid Endpoint!
      */
-    if (!lhs->online())
-        return false;
+    if (lhs->online() && rhs->online()) {
+        if (lhs->unit_size() == rhs->unit_size())
+            return false; // Maintain a stable queue
 
-    if (lhs->unit_size() == rhs->unit_size())
-        return false; // Maintain a stable queue
+        /*
+         * Always prefer Crawlers with less work at the front of the queue
+         */
+        return lhs->unit_size() > rhs->unit_size();
+    }
 
     /*
-     * Always prefer Crawlers with less work at the front of the queue
+     * If neither are online, then...
      */
-    return lhs->unit_size() < rhs->unit_size();
+    if (!lhs->online() && !rhs->online())
+        return false; // Again, maintain a stable queue
+
+    if (!lhs->online())
+        return true;
+
+    return false;
 }
 
 } // sched
