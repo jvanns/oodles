2725b21f8bf7ff967a8e15280d647f93fec0d0d3
James Vanns <james.vanns@gmail.com>
2010-06-13 17:04:15 +0100

    utility/BreadCrumbTrail: We can now keep tally on the no. of crumbs gathered.

--- utility/BreadCrumbTrail.hpp
+++ utility/BreadCrumbTrail.hpp
@@ -15,14 +15,28 @@ class BreadCrumbTrail
         typedef std::pair<coord_t, coord_t> Point;
 
         /* Member functions/methods */
-        coord_t size() const { return trail.size(); }
+        BreadCrumbTrail() : crumbs(0) {}
+        size_t gathered() const { return crumbs; }
         bool empty() const { return trail.empty(); }
-        void clear() { while(!empty()) trail.pop(); }
+        coord_t size() const { return trail.size(); }
+        void clear() { while(!empty()) trail.pop(); crumbs = 0; }
 
-        void drop_crumb(coord_t x, coord_t y) { trail.push(Point(x, y)); }
-        Point gather_crumb() { Point p = next(); trail.pop(); return p; }
+        void drop_crumb(coord_t x, coord_t y)
+        {
+            trail.push(Point(x, y));
+        }
+
+        Point gather_crumb()
+        {
+            Point p = next();
+            trail.pop();
+            ++crumbs;
+            
+            return p;
+        }
     private:
         /* Member variables/attributes */
+        size_t crumbs;
         std::queue<Point> trail;
         
         /* Member functions/methods */
