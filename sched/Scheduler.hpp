ba4e56f8b1d8521bbde3ba90f31bf8b2c2d529fd
James Vanns <james.vanns@gmail.com>
2010-06-28 19:02:25 +0100

    sched/Scheduler: Add comment desc. Signals Slots usage for Crawler/Scheduler.

--- sched/Scheduler.hpp
+++ sched/Scheduler.hpp
@@ -30,6 +30,14 @@ class Scheduler
         void schedule_from_seed(const std::string &url);
         void schedule_from_crawl(const std::string &url);
 
+        /*
+         * NOTE: Look at boost.signals for publish/subscribe events system.
+         * Using signals + slots we can quickly execute callbacks when
+         * events external to the Scheduler happen. For example, the receipt
+         * of a crawl confirmation message will contain the page ID and with
+         * this we can weigh_tree_branch() again from after retrieving the
+         * node by the ID via our hashtable, page_table.
+         */
         uint32_t run(BreadCrumbTrail *t = NULL); // Performs a scheduling run
     private:
         /* Member variables/attributes */
