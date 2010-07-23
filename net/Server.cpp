1eaef108de8ade9d898f51f464c446fe30cea523
James Vanns <james.vanns@gmail.com>
2010-07-23 08:17:31 +0100

    net/Server: Add new Server class that accepts incoming requests from peers
    New Endpoints are created upon successful connection and with the given
    ProtocolCreator a new Protocol is created and handed to the Endpoint
    before beginning transfers over that socket.

---
+++ net/Server.cpp
@@ -0,0 +1,163 @@
+// oodles
+#include "Server.hpp"
+#include "ProtocolHandler.hpp"
+#include "ProtocolCreator.hpp"
+
+// Boost.bind (TR1 is incompatible)
+#include <boost/bind.hpp>
+
+// Boost.asio
+#include <boost/asio/placeholders.hpp>
+
+// libc
+#include <limits.h> // For USHRT_MAX
+#include <stdlib.h> // For atoi()
+
+// STL
+using std::string;
+
+// Boost.bind
+using boost::bind;
+
+// Boost.system
+using boost::system::error_code;
+
+// Boost.asio
+using boost::asio::io_service;
+using boost::asio::ip::address;
+typedef boost::asio::ip::tcp::endpoint endpoint;
+typedef boost::asio::ip::tcp::acceptor acceptor;
+typedef boost::asio::ip::tcp::resolver resolver;
+
+/*
+ * Parses the ip:port formatted string and returns a Boost.asio endpoint
+ */
+static
+endpoint
+endpoint_from_service(const string &service)
+throw (oodles::net::InvalidService)
+{
+    const string::size_type split = service.find_first_of(':');
+
+    if (split == string::npos)
+        throw oodles::net::InvalidService("endpoint_from_service",
+                                          0,
+                                          "Service not in ip:port format.");
+
+    const address a(address::from_string(service.substr(0, split)));
+    const int p = atoi(service.substr(split + 1, service.size()).c_str());
+
+    if (p > USHRT_MAX)
+        throw oodles::net::InvalidService("endpoint_from_service",
+                                          0,
+                                          "Port number must be less than %u.",
+                                          USHRT_MAX);
+
+    return endpoint(a, p);
+}
+
+namespace oodles {
+namespace net {
+
+// Save my fingers!
+namespace placeholders = boost::asio::placeholders;
+
+Server::Server(io_service &s, const ProtocolCreator &c) :
+    reverse_lookup(false),
+    protocol_creator(c),
+    acceptor(s),
+    resolver(s)
+{
+}
+
+void
+Server::start(const string &service)
+throw (InvalidService)
+{
+    const endpoint e(endpoint_from_service(service));
+    error_code c;
+
+    /*
+     * Always open the socket before even setting the options
+     */
+    acceptor.open(e.protocol());
+
+    acceptor.set_option(acceptor::reuse_address(true));
+    acceptor.bind(e, c);
+
+    if (c)
+        throw InvalidService("Server::start",
+                             errno,
+                             "Failed to bind to given service; %s.",
+                             service.c_str());
+
+    acceptor.listen();
+    async_accept();
+}
+
+void
+Server::stop()
+{
+    resolver.cancel();
+    acceptor.close();
+}
+
+void
+Server::async_accept()
+{
+    Endpoint::Connection c(Endpoint::create(acceptor.get_io_service()));
+
+    acceptor.async_accept(c->socket(), bind(&Server::acceptor_callback,
+                                            this,
+                                            placeholders::error,
+                                            c));
+}
+
+void
+Server::async_resolve(Endpoint::Connection c)
+{
+    const endpoint &e = c->socket().remote_endpoint();
+
+    resolver.async_resolve(e, bind(&Server::resolver_callback,
+                                   this,
+                                   placeholders::error,
+                                   placeholders::iterator));
+}
+
+void
+Server::detatch_client(Endpoint::Connection c) const
+{
+    Endpoint::Protocol handler(protocol_creator.create());
+
+    c->set_protocol(handler);
+    c->start();
+}
+
+void
+Server::acceptor_callback(const error_code &e, Endpoint::Connection c)
+{
+    if (!e) {
+        if (reverse_lookup)
+            async_resolve(c);
+        else
+            detatch_client(c);
+
+        async_accept();
+    } else {
+        if (e != boost::asio::error::operation_aborted)
+            stop();
+    }
+}
+
+void
+Server::resolver_callback(const error_code &e, resolver::iterator i)
+{
+    if (!e) {
+    } else {
+        if (e != boost::asio::error::operation_aborted)
+            stop();
+    }
+}
+
+} // net
+} // oodles
---
+++ net/Server.hpp
@@ -0,0 +1,61 @@
+#ifndef OODLES_NET_SERVER_HPP
+#define OODLES_NET_SERVER_HPP
+
+// oodles
+#include "Endpoint.hpp"
+#include "common/Exceptions.hpp"
+
+namespace oodles {
+namespace net {
+
+class ProtocolCreator; // Forward declaration for Server
+
+class Server
+{
+    public:
+        /* Member functions/methods */
+        Server(boost::asio::io_service &s, const ProtocolCreator &c);
+
+        void start(const std::string &service) throw (InvalidService);
+        void stop();
+    private:
+        /* Member variables/attributes */
+        bool reverse_lookup;
+        const ProtocolCreator &protocol_creator;
+        boost::asio::ip::tcp::acceptor acceptor;
+        boost::asio::ip::tcp::resolver resolver;
+
+        /* Member functions/methods */
+        Server(const Server &s);
+        Server& operator= (const Server &s);
+
+        /*
+         * Asynchronous operations - registers the handlers/callbacks with
+         * the io_service which are executed (the handlers) when an accept or
+         * resolution has been completed on our behalf by the OS.
+         */
+        void async_accept();
+        void async_resolve(Endpoint::Connection c);
+        void detatch_client(Endpoint::Connection c) const;
+
+        /*
+         * Handler executed by io_service when accept operation is performed.
+         * e denotes an error code should the accept have failed
+         * c is the connected endpoint upon success
+         */
+        void acceptor_callback(const boost::system::error_code &e,
+                                          Endpoint::Connection c);
+
+        /*
+         * Handler executed by io_service when accept operation is performed.
+         * e denotes an error code should the accept have failed
+         * i is the first of n entries the resolver returned upon success
+         */
+        void resolver_callback(const boost::system::error_code &e,
+                               boost::asio::ip::tcp::resolver::iterator i);
+};
+
+} // net
+} // oodles
+
+#endif
