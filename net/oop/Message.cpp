b6cf0cf123a9edf79d37a0cb5d1b01fdb3a08362
James Vanns <james.vanns@gmail.com>
2010-08-26 19:41:51 +0100

    net/oop/Message: Introduce Message - the base class for all messages.
    Message provides the interface for determining the message type
    (it's ID from id_t), header sizes and content and virtual methods
    used by the serialiser.

--- net/oop/Message.cpp
+++ net/oop/Message.cpp
@@ -1,28 +1,59 @@
 // oodles
 #include "Message.hpp"
 
+// libc
+#include <string.h> // For memcpy()
+
 namespace oodles {
 namespace net {
 namespace oop {
 
-Message::Message()
+const uint8_t Message::Header::header_size = sizeof(uint32_t) + sizeof(id_t);
+
+Message::Message(id_t id, uint32_t body_size)
 {
+    header.message_id = id;
+    header.body_size = body_size;
 }
 
 Message::~Message()
 {
 }
 
-Message::sz_t
-Message::body_size() const
-{
-    return 0;
-}
-
 id_t
 Message::id() const
 {
-    return 0;
+    return INVALID_ID;
+}
+
+void
+Message::header2buffer(char *buffer, Header &h)
+{
+    static const uint8_t size_width = sizeof(uint32_t), id_width = sizeof(id_t);
+    const uint32_t message_size = htonl(h.header_size + h.body_size);
+    const id_t message_id = htons(h.message_id);
+
+    memcpy(buffer, &message_size, size_width);
+    memcpy(buffer + size_width, &message_id, id_width);
+    h.processed = true;
+}
+
+Message::Header
+Message::buffer2header(const char *buffer)
+{
+    static const uint8_t size_width = sizeof(uint32_t), id_width = sizeof(id_t);
+    uint32_t message_size = 0;
+    id_t message_id = 0;
+    Header h;
+    
+    memcpy(&message_size, buffer, size_width);
+    memcpy(&message_id, buffer + size_width, id_width);
+
+    h.body_size = ntohl(message_size) - h.header_size;
+    h.message_id = ntohs(message_id);
+    h.processed = true;
+
+    return h;
 }
 
 } // oop
--- net/oop/Message.hpp
+++ net/oop/Message.hpp
@@ -3,6 +3,7 @@
 
 // oodles
 #include "id_t.hpp"
+#include "common/Exceptions.hpp"
 
 namespace oodles {
 namespace net {
@@ -12,17 +13,42 @@ class Message
 {
     public:
         /* Dependent typedefs */
-        typedef uint32_t sz_t;
+        struct Header {
+            static const uint8_t header_size;
+            uint32_t body_size;
+            id_t message_id;
+            bool processed;
+
+            Header() : body_size(0), message_id(INVALID_ID), processed(false) {}
+        };
 
         /* Member functions/methods */
-        Message();
+        Message(id_t id = INVALID_ID, uint32_t body_size = 0);
         virtual ~Message();
         
-        sz_t body_size() const;
-        sz_t size() const { return header_size() + body_size(); }
-        static uint16_t header_size() { return sizeof(id_t) + sizeof(sz_t); }
-
+        static Header buffer2header(const char *buffer);
+        static void header2buffer(char *buffer, Header &h);
+        
+        /*
+         * Cast operators for casting up the class hierarchy to
+         * our concrete, derived messages.
+         */
+        template<typename Type>
+        operator Type&() { return static_cast<Type&>(*this); }
+        
         virtual id_t id() const = 0;
+        virtual Message* create(uint32_t body_size = 0) const = 0;
+        
+        virtual void reconstruct() throw(ReadError) = 0;
+        virtual void deconstruct() throw(WriteError) = 0;
+        
+        virtual size_t to_buffer(char *buffer, size_t max) = 0;
+        virtual size_t from_buffer(const char *buffer, size_t max) = 0;
+        
+        size_t size() const { return header.header_size + header.body_size; }
+    protected:
+        /* Member variables/attributes */
+        Header header;
 };
 
 } // oop
