e7060919bbb14bcc0a3b42ad699088c8d3a8ddad
James Vanns <james.vanns@gmail.com>
2010-08-30 09:45:13 +0100

    net/core/ProtocolCreator: Add new base class of 'dialect'.
    A protocol may, depending on component and context, have different dialects.
    Of course, this certainly isn't true of simple client/server systems so we
    make dialect an optional 2nd template parameter to Protocol (which still
    inherits from ProtocolCreator).
    
    If no dialect is needed the derived ProtocolHandler will still suffice as
    before - it to however (the base class that is), now inherits from
    ProtocolDialect.

--- net/core/ProtocolCreator.hpp
+++ net/core/ProtocolCreator.hpp
@@ -1,13 +1,74 @@
 #ifndef OODLES_NET_PROTOCOLCREATOR_HPP
 #define OODLES_NET_PROTOCOLCREATOR_HPP
 
-// Boost.shared_ptr
-#include <boost/shared_ptr.hpp>
+// STL
+#include <typeinfo>
+
+// libc
+#include <stdio.h> // For NULL
 
 namespace oodles {
 namespace net {
 
 struct ProtocolHandler; // Forward declaration for ProtocolCreator
+
+struct ProtocolDialect
+{
+    /* Member functions/methods */
+    ProtocolDialect() : handler(NULL) {}
+    virtual ~ProtocolDialect() {}
+
+    void set_handler(ProtocolHandler &p) { handler = &p; }
+
+    /*
+     * Cast operators for casting up the class hierarchy to
+     * our concrete, derived subclass.
+     */
+    template<typename Dialect>
+    operator Dialect&() { return static_cast<Dialect&>(*this); }
+
+    /* Member variables/attributes */
+    ProtocolHandler *handler; // The link to our protocol handler
+};
+
+struct NoDialect : public ProtocolDialect
+{
+};
+
+class DialectCreator
+{
+    public:
+        /* Member functions/methods */
+        DialectCreator() {}
+        virtual ~DialectCreator() {}
+        virtual ProtocolDialect* create(ProtocolHandler &h) const = 0;
+    private:
+        /* Member functions/methods */
+        DialectCreator(const DialectCreator &p);
+        DialectCreator& operator= (const DialectCreator &p);
+};
+
+template<typename Dialect> struct ProtocolInterpreter : public DialectCreator
+{
+    public:
+        /* Member functions/methods */
+        ProtocolInterpreter() :
+            no_dialect(typeid(NoDialect)),
+            chosen_dialect(typeid(Dialect))
+        {
+        }
+        
+        ProtocolDialect* create(ProtocolHandler &h) const
+        {
+            if (chosen_dialect == no_dialect)
+                return &h;
+            
+            return new Dialect;
+        }
+    private:
+        /* Member variables/attributes */
+        const std::type_info &no_dialect, &chosen_dialect;
+};
 
 class ProtocolCreator
 {
@@ -17,30 +78,21 @@ class ProtocolCreator
         virtual ~ProtocolCreator() {}
         virtual ProtocolHandler* create() const = 0;
     private:
+        /* Member functions/methods */
         ProtocolCreator(const ProtocolCreator &p);
         ProtocolCreator& operator= (const ProtocolCreator &p);
 };
 
-template<typename DerivedHandler> class Protocol : public ProtocolCreator
+template<typename Handler, typename Dialect = NoDialect>
+class Protocol : public ProtocolCreator
 {
-    private:
-        /* Dependent typedefs */
-        typedef boost::shared_ptr<ProtocolHandler> Base;
     public:
-        /* Dependent typedefs */
-        typedef boost::shared_ptr<DerivedHandler> Handler;
-
         /* Member functions/methods */
-        ProtocolHandler* create() const
-        {
-           return new DerivedHandler;
-        }
-
-        Handler handler(const Base &b) const
-        {
-            return boost::static_pointer_cast
-                   <DerivedHandler, ProtocolHandler>(b);
-        }
+        Protocol() {}
+        ProtocolHandler* create() const { return new Handler(interpreter); }
+    private:
+        /* Member variables/attributes */
+        const ProtocolInterpreter<Dialect> interpreter;
 };
 
 } // net
