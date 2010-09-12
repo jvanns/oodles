b59e4ee9ce1ea6459e01b14f1d2b3eed4414eb79
James Vanns <james.vanns@gmail.com>
2010-09-12 09:45:52 +0100

    net/oop/Messages: Add the definition of the two new messages, Begin/End Crawl.

--- net/oop/Messages.cpp
+++ net/oop/Messages.cpp
@@ -1,12 +1,28 @@
 // oodles
 #include "Messages.hpp"
-#include "Serialiser.hpp"
 
 // Boost.serialization
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/list.hpp>
 #include <boost/serialization/string.hpp>
 
 // STL
 using std::string;
+
+namespace {
+
+bool init()
+{
+    oodles::net::oop::RegisterCrawler::init();
+    oodles::net::oop::BeginCrawl::init();
+    oodles::net::oop::EndCrawl::init();
+    
+    return true;
+}
+
+static const bool factory_initialised = init();
+
+} // anonymous
 
 namespace oodles {
 namespace net {
@@ -27,6 +43,36 @@ RegisterCrawler_::serialize(Deconstructor &archive,
 {
     archive << cores;
     archive << name;
+}
+
+void
+BeginCrawl_::serialize(Reconstructor &archive,
+                       unsigned int /* version */)
+{
+    archive >> urls;
+}
+
+void
+BeginCrawl_::serialize(Deconstructor &archive,
+                       unsigned int /* version */) const
+{
+    archive << urls;
+}
+
+void
+EndCrawl_::serialize(Reconstructor &archive,
+                     unsigned int /* version */)
+{
+    archive >> new_urls;
+    archive >> scheduled_urls;
+}
+
+void
+EndCrawl_::serialize(Deconstructor &archive,
+                     unsigned int /* version */) const
+{
+    archive << new_urls;
+    archive << scheduled_urls;
 }
 
 } // msg
--- net/oop/Messages.hpp
+++ net/oop/Messages.hpp
@@ -2,9 +2,12 @@
 #define OODLES_NET_OOP_MESSAGES_HPP
 
 // oodles
+#include "url/URL.hpp"
 #include "Serialiser.hpp"
 
 // STL
+#include <map>
+#include <list>
 #include <string>
 
 // libc
@@ -34,9 +37,44 @@ struct RegisterCrawler_
     std::string name;
 };
 
+struct BeginCrawl_
+{
+    /* Dependent typedefs */
+    //................Page ID...........Page URL
+    typedef std::pair<url::URL::hash_t, std::string> URL;
+    
+    /* Member functions/methods */
+    static id_t id() { return BEGIN_CRAWL; }
+    void serialize(Reconstructor &archive, unsigned int /* version */);
+    void serialize(Deconstructor &archive, unsigned int /* version */) const;
+    
+    /* Member variables/attributes */
+    //.......Domain ID.........URLs
+    std::map<url::URL::hash_t, std::list<URL> > urls;
+};
+
+struct EndCrawl_
+{
+    /* Dependent typedefs */
+    //................Page URL.....Crawled?
+    typedef std::pair<std::string, bool> URL;
+    
+    /* Member functions/methods */
+    static id_t id() { return END_CRAWL; }
+    void serialize(Reconstructor &archive, unsigned int /* version */);
+    void serialize(Deconstructor &archive, unsigned int /* version */) const;
+    
+    /* Member variables/attributes */
+    std::list<URL> new_urls;
+    //.......Page ID...........Success?
+    std::map<url::URL::hash_t, bool> scheduled_urls;
+};
+
 } // msg
 
 typedef Serialiser<msg::RegisterCrawler_> RegisterCrawler;
+typedef Serialiser<msg::BeginCrawl_> BeginCrawl;
+typedef Serialiser<msg::EndCrawl_> EndCrawl;
 
 } // oop
 } // net
