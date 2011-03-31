933b18b3e0ac3117ed0e4284b98a6c1974ba028d
James Vanns <james.vanns@gmail.com>
2011-03-31 14:09:39 +0100

    net/http/Session: Add bare bones Session base class.
    Defines a simple interface for HTTP message passing over a network.

---
+++ net/http/Session.cpp
@@ -0,0 +1,28 @@
+// oodles
+#include "Session.hpp"
+#include "Protocol.hpp"
+
+namespace oodles {
+namespace net {
+namespace http {
+
+void
+Session::handle_messages()
+{
+    Protocol &p = *static_cast<Protocol*>(get_endpoint()->get_protocol());
+    
+    for (Message *m = p.pop_message() ; m ; m = p.pop_message())
+        handle_message(m);
+}
+
+void
+Session::push_message(Message *m)
+{
+    Protocol &p = *static_cast<Protocol*>(get_endpoint()->get_protocol());
+    p.push_message(m);
+}
+
+} // http
+} // net
+} // oodles
+
---
+++ net/http/Session.hpp
@@ -0,0 +1,27 @@
+#ifndef OODLES_NET_HTTP_SESSION_HPP
+#define OODLES_NET_HTTP_SESSION_HPP
+
+// oodles
+#include "utility/Linker.hpp"
+#include "net/core/SessionHandler.hpp"
+
+namespace oodles {
+namespace net {
+namespace http {
+
+class Message; // Forward declaration for Session
+
+class Session : public Linker, public SessionHandler
+{
+    public:
+        void handle_messages();
+        
+        void push_message(Message *m); // Ownership transferred
+        virtual void handle_message(Message *m) = 0; // Ownership transferred
+};
+   
+} // http
+} // net
+} // oodles
+
+#endif
