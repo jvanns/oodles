47a1481d681457f9eed3856475d0038e8b6cdb82
James Vanns <james.vanns@gmail.com>
2011-04-15 18:58:59 +0100

    url/Parser: Replace boost spirit based parser in favour of easier bespoke one!
    Revert to the older (wow - a year old!?) bespoke URL parser. It is simpler to
    understand and bugfix, leaner, quicker to compile (I develop on a 12" G4
    PowerBook mostly!) and probably faster too.
    
    The main reason for this is that my spirit parser grammar was incorrect and
    couldn't distinguish between a short hostname (no domain suffix) followed by
    a port number or a username + password. Whats worse is I couldn't find out how
    to fix it!
    
    It's only a URL afterall - no need for something as full-featured as Spirit
    here, I don't think!

--- url/Parser.hpp
+++ url/Parser.hpp
@@ -1,55 +1,34 @@
 #ifndef OODLES_URL_PARSER_HPP
 #define OODLES_URL_PARSER_HPP
 
-// oodles
-#include "type.hpp"
-
-// Boost.spirit
-#include <boost/spirit/include/qi.hpp>
+// STL
+#include <string>
+#include <vector>
 
 namespace oodles {
 namespace url {
 
-namespace qi = boost::spirit::qi;
-
 struct Attributes
 {
     bool ip; // Is the domain stored in fact an IP address?
-
-    value_type port,
-               page,
-               scheme,
-               username,
-               password;
-    std::vector<value_type> path, domain;
-
-    Attributes() : ip(false) {} // Assume not to begin with
+    
+    std::string port,
+                page,
+                scheme,
+                username,
+                password;
+    std::vector<std::string> path, domain;
+    
+    Attributes() : ip(false) {}
 };
 
-struct Parser : qi::grammar<value_type::const_iterator, Attributes()>
+struct Parser
 {
-    Parser(); // Definition of the grammar is constructed here
-    
-    bool parse(value_type::const_iterator &begin,
-               value_type::const_iterator &end,
-               Attributes &url);
-    /*
-     * There is no reason for these to be public really.
-     */
-    private:
-        bool ip;
-        qi::rule<value_type::const_iterator, Attributes()> url;
-        qi::rule<value_type::const_iterator, value_type()> dir,
-                                                           page,
-                                                           port,
-                                                           scheme,
-                                                           username,
-                                                           password;
-        qi::rule<value_type::const_iterator, std::vector<value_type>()> path,
-                                                                        domain;
+    bool parse(const std::string &url, Attributes &attributes);
 };
 
 } // url
 } // oodles
 
 #endif
+
